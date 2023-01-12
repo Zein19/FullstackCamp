402bff1a43a881d1c99a46083a1efd578fb8a404
Zein19 <zein.23@hotmail.co.uk>
2023-01-12 18:38:33 +0000

    Javascript homework

---
+++ Javascript/Javascript-HW1/HW1.js
@@ -0,0 +1,36 @@
+//question 1
+console.log ("hello "+"world ")
+
+//question 2
+var color = "blue"
+var size = "big"
+var type = "house"
+var num = 4
+
+console.log(size + color + type + num)
+
+//question 3
+console.log("I have"+num +"brothers and a "+color+ type)
+
+//question 4
+console.log(typeof num)
+console.log(typeof color)
+
+//question 5
+var array = ["blue", "red", "yellow", "green", "orange", "pink", "purple", "brown", "black", "white", "aqua", "grey", "maroon", "violet", "navy"]
+console.log(array)
+
+//question 6
+array.pop()
+
+console.log(array)
+
+//question 7
+array.push("indigo")
+console.log(array)
+
+//question 8
+console.log(array[0])
+console.log(array[7])
+console.log(array[14])
+
---
+++ Javascript/hw2.js
@@ -0,0 +1,56 @@
+//q1
+var Batman = "DC"
+
+if(Batman === "Marvel") {
+    console.log("This is a Marvel hero")
+} 
+else{
+    console.log("This is a DC hero")
+}
+
+//q2
+var age = 10
+
+if(age >= 5 && age < 12){
+    console.log("They are in primary school")
+}
+else if(age >= 12 && age < 17){
+    console.log("They are in secondary school")
+}
+else if(age >= 17 && age <=18){
+    console.log("They are in college")
+}
+else{
+    console.log("They are in optional education")
+}
+
+//q3
+var shopping =["milk","bread","cheese","butter","eggs"]
+
+if(shopping.length === 5){
+    console.log("Valid")
+}
+else{
+    console.log("Error")
+}
+
+//q4
+var country="Somalia"
+
+if( country.toLowerCase() === "somalia"){
+    console.log("I am from "+country)
+}
+else{
+    console.log("I'm not from there")
+}
+
+//q5
+var phone ="iPhone"
+
+if(phone ==="iPhone"){
+    console.log(phone+" user")
+}
+else{
+    console.log("Android user")
+}
+
