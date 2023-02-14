24ee199dd7441c4a51e48dfaadc5755d14875aac
Zein19 <zein.23@hotmail.co.uk>
2023-02-14 16:26:12 +0000

    practicing react components

---
+++ React/practice-react/src/Footer.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Footer(){
+    return(
+        <ul className='footer'>
+            <li className="App-link">Twitter</li>
+            <li className="App-link">Facebook</li>
+            <li className="App-link">Instagram</li>
+        </ul>
+    )
+}
+
+export default Footer
---
+++ React/practice-react/src/Header.js
@@ -0,0 +1,12 @@
+import './App.css';
+function Header(){
+    return(
+        <ul className='header'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+        </ul>
+    )
+}
+
+export default Header
