8ae191cd4cc235f1d84682406154b89eb8a92dfc
Zein19 <zein.23@hotmail.co.uk>
2023-02-22 14:32:01 +0000

    made card component and imported data

--- React/premier-league/src/App.js
+++ React/premier-league/src/App.js
@@ -1,11 +1,26 @@
 import Header from './components.js/Header';
+import Clubs from './Data';
+import Cards from './components.js/Cards';
 import './App.css';
 
 function App() {
+  const team = Clubs.map(club=>{
+    return(
+      <Cards
+      name={club.name}
+      logo={club.logo}
+      stadium={club.stadium} 
+      />
+    )
+  })
+
   return (
     <div className="App">
       <Header />
-      
+      <div className='flex'>
+        {team}
+      </div>
+
     </div>
   );
 }
---
+++ React/premier-league/src/Data.js
@@ -0,0 +1,147 @@
+ 
+  const Clubs = [
+    {
+        "name": "Arsenal FC",
+        "code": "ARS",
+        "country": "England",
+        "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+        "stadium": "Emirates Stadium"
+    },
+    {
+        "name": "Aston Villa FC",
+        "code": "AVL",
+        "country": "England",
+        "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+        "stadium": "Villa Park"
+    },
+    {
+      "name": "Bournemouth FC",
+      "code": "BOU",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Vitality Stadium"
+    },
+    {
+      "name": "Manchester United FC",
+      "code": "MUN",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Old Trafford"
+    },
+    {
+      "name": "Manchester City FC",
+      "code": "MCI",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Etihad Stadium"
+    },
+    
+    {
+      "name": "Fulham FC",
+      "code": "FUL",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Craven Cottage"
+    },
+    
+    {
+      "name": "Crystal Palace FC",
+      "code": "CRY",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Selhurst Park"
+    },
+    {
+      "name": "Southampton FC",
+      "code": "SOU",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "St Mary's Stadium"
+    },
+    {
+      "name": "Liverpool FC",
+      "code": "LIV",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Anfield"
+    },
+    {
+      "name": "Leeds United FC",
+      "code": "LEE",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Elland Road"
+    },
+    {
+      "name": "West Ham United FC",
+      "code": "WHU",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "London Stadium"
+    },
+    {
+      "name": "Newcastle United FC",
+      "code": "NEW",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "St James' Park"
+    },
+    {
+      "name": "Nottingham Forest FC",
+      "code": "NFC",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "The City Ground"
+    },
+    {
+      "name": "Leicester City FC",
+      "code": "LEI",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "King Power Stadium"
+    },
+    {
+      "name": "Tottenham Hotspur FC",
+      "code": "TOT",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Tottenham Hotspur Stadium"
+    },
+    {
+      "name": "Everton FC",
+      "code": "EVE",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Goodison Park"
+    },
+    {
+      "name": "Brentford FC",
+      "code": "BRE",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Gtech Community Stadium"
+    },
+    {
+      "name": "Wolverhampton Wanderers FC",
+      "code": "WOL",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Molineux Stadium"
+    },
+    {
+      "name": "Brighton & Hove Albion FC",
+      "code": "BHA",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Amex Stadium"
+    },
+    {
+      "name": "Chelsea FC",
+      "code": "CHE",
+      "country": "England",
+      "logo": "https://resources.premierleague.com/premierleague/badges/t91.svg",
+      "stadium": "Stamford Bridge"
+    }
+  ]
+
+  export default Clubs
